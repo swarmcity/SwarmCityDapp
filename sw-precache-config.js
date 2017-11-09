37ab8eef92a34a1dd899d470f707f7c681ba3791
@bkawk <email.will.in.china@gmail.com>
2017-11-09 12:50:30 +0800

    lint

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * This code may only be used under the BSD style license http://polymer.github.io/LICENSE.txt
  * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
  * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
  * Code distributed by Google as part of the polymer project is also
