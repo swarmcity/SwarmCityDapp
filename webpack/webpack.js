12af2d1b8bf4f6fe793146adc9f6833ddc06eb9e
Will in China <email.will.in.china@gmail.com>
2018-08-08 16:43:18 +0800

    original pack

--- webpack/webpack.js
+++ webpack/webpack.js
@@ -1,4 +1,48 @@
-import Contract from 'web3-eth-contract';
+import {
+    sha3,
+    addHexPrefix,
+    isValidAddress,
+    privateToAddress,
+    privateToPublic,
+    publicToAddress,
+    toChecksumAddress,
+    ecsign,
+    ecrecover,
+    toBuffer,
+    bufferToHex,
+    fromRpcSig} from 'ethereumjs-util';
+  import {
+    createHash,
+    randomBytes,
+    createCipheriv,
+    pbkdf2Sync,
+    createDecipheriv} from 'crypto';
+  import scrypt from 'scryptsy';
+  import uuid from 'uuid';
+  import Buffer from 'buffer';
+  import QRCode from 'qrcode';
+  import Contract from 'web3-eth-contract';
 export {
+    sha3,
+    addHexPrefix,
+    isValidAddress,
+    privateToAddress,
+    privateToPublic,
+    publicToAddress,
+    toChecksumAddress,
+    ecsign,
+    ecrecover,
+    toBuffer,
+    bufferToHex,
+    fromRpcSig,
+    createHash,
+    randomBytes,
+    createCipheriv,
+    pbkdf2Sync,
+    createDecipheriv,
+    scrypt,
+    uuid,
+    Buffer,
+    QRCode,
     Contract
 };
