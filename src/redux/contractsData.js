29c848fcafc6783bb15b95b6565cc813ff907630
Unknown <guillem.lion.benosa@gmail.com>
2018-08-30 22:42:34 +0200

    Implemented getTxs
    It is not blended with the rest of the app. To call it, open the console and call ```test()```

---
+++ src/redux/contractsData.js
@@ -0,0 +1,30 @@
+window.contractsData = {
+    swtToken: {
+        address: '0x0adbc89464cb7fa752adcf3533514949a2547589',
+        deployBlock: 7288019,
+        abi: {
+            TransferEvent: {
+                'anonymous': false,
+                'inputs': [
+                    {
+                        'indexed': true,
+                        'name': '_from',
+                        'type': 'address',
+                    },
+                    {
+                        'indexed': true,
+                        'name': '_to',
+                        'type': 'address',
+                    },
+                    {
+                        'indexed': false,
+                        'name': '_amount',
+                        'type': 'uint256',
+                    },
+                ],
+                'name': 'Transfer',
+                'type': 'event',
+            },
+        },
+    },
+};
